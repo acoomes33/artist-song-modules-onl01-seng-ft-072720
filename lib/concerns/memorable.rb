fdc0231bd82ef193d9f6d95e10bf9085f77d26a2
Alan Coomes <acoomes1@cord.edu>
2020-08-12 05:00:27 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
-  
+  module ClassMethods
     def reset_all
     all.clear
-  end
+    end
+  
+    def count
+      all.count
+    end
+  end 
 
-  def count
-    all.count
-  end
-
+  module InstanceMethods
+    def initialize
+      
+    end 
+  end 
 end 
